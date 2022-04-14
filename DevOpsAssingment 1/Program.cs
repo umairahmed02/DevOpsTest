f78160f87865ec1c7ffa479d29db8c22cd403dba
umairahmed02 <umi1045@gmail.com>
2022-04-14 13:15:42 +1200

    initial commit

---
+++ DevOpsAssingment 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevOpsAssingment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
